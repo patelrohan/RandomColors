f86332bdb8ab563b38cd6083963578ad2aec5472
Rohan Patel <patelrohan@users.noreply.github.com>
2023-09-03 23:58:25 -0400

    First commit
    Storyboard setup and TableViewController in progress

--- RandomColors/ViewController.swift
+++ RandomColors/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var colors: [UIColor] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createRandomColors()
+    }
+    
+    
+    func createRandomColors(){
+        for _ in 0..<50{
+            colors.append(addRandomColor())
+        }
     }
 
+    
+    func addRandomColor() -> UIColor{
+        let randomColor = UIColor(red: CGFloat.random(in: 0...1),
+                                  green: CGFloat.random(in: 0...1),
+                                  blue: CGFloat.random(in: 0...1),
+                                  alpha: 1.0)
+        
+        return randomColor
+    }
 
 }
 
+
+extension ViewController:  UITableViewDelegate, UITableViewDataSource{
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell")
+        
+        return UITableViewCell()
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ToColorsDetailVC", sender: nil)
+    }
+}
